293455ff641cd3419ac29ac1a19083a2d4c8eaac
Cristian Giraldo Villegas <142264484+CristianGiVi@users.noreply.github.com>
2024-12-13 13:25:12 -0500

    Primer commit
    Se ha inicialidado el proyecto

---
+++ src/main/java/cohan/backend_prueba_tecnica_cohan/BackendPruebaTecnicaCohanApplication.java
@@ -0,0 +1,13 @@
+package cohan.backend_prueba_tecnica_cohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPruebaTecnicaCohanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPruebaTecnicaCohanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cohan/backend_prueba_tecnica_cohan/BackendPruebaTecnicaCohanApplicationTests.java
@@ -0,0 +1,13 @@
+package cohan.backend_prueba_tecnica_cohan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPruebaTecnicaCohanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
